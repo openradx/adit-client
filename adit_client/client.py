5946ab124afc8eea7a55a8b0fa247779d47d6701
Kai Schlamp <kai.schlamp@gmail.com>
2024-05-28 15:49:41 +0000

    Allow to verify with ca bundle (for SSL connections using local SSL certificates, e.g. intranet)

--- adit_client/client.py
+++ adit_client/client.py
@@ -1,11 +1,12 @@
-from dicomweb_client import DICOMwebClient
+from dicomweb_client import DICOMwebClient, session_utils
 from pydicom import Dataset
 
 
 class AditClient:
-    def __init__(self, server_url: str, auth_token: str) -> None:
+    def __init__(self, server_url: str, auth_token: str, verify: str | bool = True) -> None:
         self.server_url = server_url
         self.auth_token = auth_token
+        self.verify = verify
 
     def search_for_studies(
         self, ae_title: str, query: dict[str, str] | None = None
@@ -52,7 +53,15 @@ class AditClient:
         self._create_dicom_web_client(ae_title).store_instances(instances)
 
     def _create_dicom_web_client(self, ae_title: str) -> DICOMwebClient:
+        session = session_utils.create_session()
+
+        if isinstance(self.verify, bool):
+            session.verify = self.verify
+        else:
+            session = session_utils.add_certs_to_session(session=session, ca_bundle=self.verify)
+
         return DICOMwebClient(
+            session=session,
             url=f"{self.server_url}/api/dicom-web/{ae_title}",
             qido_url_prefix="qidors",
             wado_url_prefix="wadors",
