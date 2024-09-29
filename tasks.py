e4dc0401c8e8296e1ddfc7cbe5bc7eed2fea682d
Kai Schlamp <kai.schlamp@gmail.com>
2024-09-29 17:57:03 +0000

    Setup rudimentary test and fix git tag of adit-radis-shared

--- tasks.py
+++ tasks.py
@@ -1,23 +1,31 @@
+import sys
 from pathlib import Path
 from shutil import copy
 
 from adit_radis_shared.invoke_tasks import show_outdated  # noqa: F401
 from invoke.context import Context
-from invoke.runners import Result
 from invoke.tasks import task
 
 project_dir = Path(__file__).resolve().parent
 
 
-def run_cmd(ctx: Context, cmd: str, silent=False) -> Result:
-    if not silent:
-        print(f"Running: {cmd}")
+###
+# Helper functions
+###
 
-    hide = True if silent else None
 
-    result = ctx.run(cmd, pty=True, hide=hide)
+def check_container_up(ctx: Context, container_name):
+    result = ctx.run("docker ps", hide=True, warn=True)
     assert result and result.ok
-    return result
+    for line in result.stdout.splitlines():
+        if container_name in line:
+            return True
+    return False
+
+
+###
+# Tasks
+###
 
 
 @task
@@ -37,11 +45,35 @@ def lint(ctx: Context):
 
 
 @task
-def publish_client(ctx: Context):
-    """Publish ADIT Client to PyPI
+def test(
+    ctx: Context,
+    path: str | None = None,
+    cov: bool | str = False,
+    html: bool = False,
+    keyword: str | None = None,
+    mark: str | None = None,
+    stdout: bool = False,
+    failfast: bool = False,
+):
+    """Run the test suite"""
+    if not check_container_up(ctx, "postgres"):
+        sys.exit("Tests need a running PostgreSQL database.\nRun 'docker compose up -d' first.")
 
-    - Make sure you released a new version with tag in GitHub
-    - Make sure PyPI API token is set: poetry config pypi-token.pypi your-api-token
-    - Execute with `invoke publish-client`
-    """
-    run_cmd(ctx, "poetry publish --build")
+    cmd = "pytest "
+    if cov:
+        cmd += "--cov "
+        if isinstance(cov, str):
+            cmd += f"={cov} "
+        if html:
+            cmd += "--cov-report=html"
+    if keyword:
+        cmd += f"-k {keyword} "
+    if mark:
+        cmd += f"-m {mark} "
+    if stdout:
+        cmd += "-s "
+    if failfast:
+        cmd += "-x "
+    if path:
+        cmd += path
+    ctx.run(cmd, pty=True)
